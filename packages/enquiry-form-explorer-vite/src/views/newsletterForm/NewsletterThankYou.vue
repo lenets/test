60185d735ec8db80a3bd6359b82a6b773658e70c
testset1231 <104201069+testset1231@users.noreply.github.com>
2023-05-30 21:18:53 +0300

    Update NewsletterThankYou.vue

--- packages/enquiry-form-explorer-vite/src/views/newsletterForm/NewsletterThankYou.vue
+++ packages/enquiry-form-explorer-vite/src/views/newsletterForm/NewsletterThankYou.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
 import NewsletterWrapper from '@/components/NewsletterWrapper.vue';
-
-const footerLinks = [
-  {
-    name: 'Alle Reisen',
-    href: 'https://www.explorer.de/reiseziele.html'
-  },
-  {
-    name: 'Mietwagen / Camper',
-    href: 'https://www.explorer.de/mietwagen-camper.html',
-  },
-  { name: 'Hotels', href: 'https://www.explorer.de/reisearten/hotels.html' },
-  { name: 'Flüge', href: 'https://www.explorer.de/fluege.html' },
-  { name: 'Zur Homepage', href: 'https://www.explorer.de/' },
-];
+import { getSiteData } from '@/helpers';
+const siteData = getSiteData();
 </script>
 
 <template>
   <NewsletterWrapper>
     <div>
-      <h1 class="text-primary font-bold font-secondary text-22 mb-24 mt-40">
+      <h1 class="step-title mb-24 mt-46">
         Schön, dass du dabei bist!
       </h1>
       <div class="font-secondary text-16">
@@ -29,54 +17,50 @@ const footerLinks = [
         </p>
         <p class="mt-24">Wir freuen uns darauf, dich für einzigartige Reise-Erlebnisse zu begeistern.</p>
       </div>
-      <h2 class="mt-48 font-secondary text-18 font-semibold">Vielleicht findest du schon eine spannende Reise bei uns?</h2>
-      <div class="md:flex mt-24 mb-24 justify-between">
-        <div class="p-24 bg-grey-lighter rounded-3xl w-full md:w-[48%]">
-          <div class="h-160 mb-24">
-            <img
-              class="w-full h-full object-cover rounded-[8px] max-w-[250px] ml-auto mr-auto"
-              src="@/assets/specialOffersPhoto-2.jpg"
-              alt="Special Offers Photo"
-            />
-          </div>
-          <p class="font-secondary font-medium tracking-[.5px] text-lightgrey text-16 my-24">
-            Wir helfen Dir günstig, die schönsten Reise-Momente zu erleben.
-            Mit immer neuen Angeboten.
-          </p>
-          <a href="https://www.explorer.de/aktionen.html"
-            ><div
-              class="primary-btn"
-            >
-              Aktionsangebote
+      <div class="cards">
+        <h2 class="cards__title font-secondary text-primary-dark text-18 font-semibold">Vielleicht findest du schon eine spannende Reise bei uns?</h2>
+        <div class="md:flex mt-24 mb-24 justify-between">
+          <div class="card">
+            <div class="h-160 mb-24">
+              <img
+                class="w-full h-full object-cover rounded-[8px] max-w-[250px] ml-auto mr-auto"
+                src="@/assets/specialOffersPhoto-2.jpg"
+                alt="Special Offers Photo"
+              />
             </div>
-          </a>
-        </div>
-        <div
-          class="p-24 bg-grey-lighter rounded-3xl w-full md:w-[48%] mt-24 md:mt-0"
-        >
-          <div class="h-160 mb-24 rounded-2xl">
-            <img
-              class="w-full h-full object-cover rounded-[8px] max-w-[250px] ml-auto mr-auto"
-              src="@/assets/travelBlogsPhoto-2.jpg"
-              alt="Travel Blogs Photo"
-            />
+            <p class="card__txt font-secondary font-medium tracking-[.5px] text-lightgrey text-16 my-24">
+              Wir helfen Dir günstig, die schönsten Reise-Momente zu erleben.
+              Mit immer neuen Angeboten.
+            </p>
+            <a href="https://www.explorer.de/aktionen.html">
+              <div class="primary-btn">
+                  Aktionsangebote
+              </div>
+            </a>
           </div>
-          <p class="font-secondary font-medium tracking-[.5px] text-lightgrey text-16 my-24">
-            Unsere Reiseberater reisen passioniert. Hier teilen sie ihre 
-            Erfahrungen und Erlebnisse.
-          </p>
-          <a href="https://www.explorer.de/kampagnen/reiseberichte.html"
-            ><div
-              class="primary-btn"
-            >
-              Reiseberichte
-            </div></a
-          >
+          <div class="card">
+            <div class="h-160 mb-24 rounded-2xl">
+              <img
+                class="w-full h-full object-cover rounded-[8px] max-w-[250px] ml-auto mr-auto"
+                src="@/assets/travelBlogsPhoto-2.jpg"
+                alt="Travel Blogs Photo"
+              />
+            </div>
+            <p class="card__txt font-secondary font-medium tracking-[.5px] text-lightgrey text-16 my-24">
+              Unsere Reiseberater reisen passioniert. Hier teilen sie ihre 
+              Erfahrungen und Erlebnisse.
+            </p>
+            <a href="https://www.explorer.de/kampagnen/reiseberichte.html">
+              <div class="primary-btn">
+                Reiseberichte
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-      <div class="flex gap-[16px] gap-y-[24px] justify-center flex-wrap mb-120">
-        <div class="text-primary" v-for="link in footerLinks" :key="link.name">
-          <a class="footer-link checked-fill font-medium" :href="link.href">{{ link.name }}</a>
+      <div class="footer-links flex gap-[16px] gap-y-[24px] justify-center flex-wrap mb-120">
+        <div class="text-primary" v-for="link in siteData.thankYouLinks" :key="link.link">
+          <a class="footer-link checked-fill font-medium" :href="link.link">{{ link.text }}</a>
         </div>
       </div>
     </div>
@@ -84,6 +68,34 @@ const footerLinks = [
 </template>
 
 <style scoped>
+.stattravel .footer-links {
+  @apply max-w-[475px] mx-auto;
+}
+.footer-link {
+  @apply inline-block rounded-full py-12 px-18 font-primary text-primary leading-4 text-14 bg-white ring-inset ring-1 ring-primary;
+}
+
+.stattravel .footer-link {
+  @apply rounded-lg font-semibold;
+}
+.stattravel .cards__title {
+  @apply text-22;
+}
+.card {
+  @apply p-24 bg-grey-lighter rounded-3xl w-full md:w-[48%] mt-24 md:mt-0;
+}
+
+.stattravel .card {
+  @apply p-16 bg-[#F8F8F9];
+}
+
+.card__txt {
+  @apply text-lightgrey;
+}
+
+.stattravel .card__txt {
+  @apply text-primary-dark min-h-[96px] flex items-center;
+}
 .primary-btn {
   display: flex;
   align-items: center;
