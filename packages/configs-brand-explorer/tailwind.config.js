b58f5d753058e2673dd988da57a4b8edd20e4bfa
testset1231 <104201069+testset1231@users.noreply.github.com>
2023-05-30 18:14:57 +0300

    5

--- packages/configs-brand-explorer/tailwind.config.js
+++ packages/configs-brand-explorer/tailwind.config.js
@@ -2,21 +2,49 @@ const {
   getTailwindConfig: _getConfig,
 } = require('configs-common/tailwind.config');
 
-function getTailwindConfig({ purge }) {
+function getTailwindConfig({ purge, site }) {
+  let schema = {};
+  switch (site) {
+    case 'stattravel':
+      schema = {
+        brandStyles: {
+          colorPrimary: '#0065BD',
+          colorPrimaryLight: '#007353',
+          colorPrimaryLighter: '#0065bd66',
+          colorPrimaryDark: '#1B365D',
+
+          colorSecondary: '#D24701',
+          colorSecondaryLight: '#D24701',
+          colorSecondaryLighter: '#D24701',
+
+          colorThird: '#D9D9D9',
+        },
+        brandFontPrimary: 'Titillium Web',
+        brandFontSecondary: 'Titillium Web',
+      }
+      break;
+
+    default:
+      schema = {
+        brandStyles: {
+          colorPrimary: '#008560',
+          colorPrimaryLight: '#007353',
+          colorPrimaryLighter: '#0085604d',
+          colorPrimaryDark: '#000',
+
+          colorSecondary: '#D24701',
+          colorSecondaryLight: '#D24701',
+          colorSecondaryLighter: '#D24701',
+
+          colorThird: '#D9D9D9',
+        },
+        brandFontPrimary: 'Nunito',
+        brandFontSecondary: 'Montserrat',
+      }
+      break;
+  }
   return _getConfig({
-    brandStyles: {
-      colorPrimary: '#008560',
-      colorPrimaryLight: '#007353',
-      colorPrimaryLighter: '#169D7C',
-
-      colorSecondary: '#D24701',
-      colorSecondaryLight: '#D24701',
-      colorSecondaryLighter: '#D24701',
-
-      colorThird: '#D9D9D9',
-    },
-    brandFontPrimary: 'Nunito',
-    brandFontSecondary: 'Montserrat',
+    ...schema,
     purge,
   });
 }
