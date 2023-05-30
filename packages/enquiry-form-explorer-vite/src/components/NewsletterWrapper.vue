7fdc21b823ca9f2f791500c0b2cd9b5917de0235
testset1231 <104201069+testset1231@users.noreply.github.com>
2023-05-30 18:29:49 +0300

    Update NewsletterWrapper.vue

--- packages/enquiry-form-explorer-vite/src/components/NewsletterWrapper.vue
+++ packages/enquiry-form-explorer-vite/src/components/NewsletterWrapper.vue
@@ -21,7 +21,7 @@
     <aside class="hidden lg:block h-screen w-1/3 sticky top-0">
       <img
         alt="Aside image"
-        src="@/assets/sidebaExr.jpg"
+        src="@/assets/sidebaEx.jpg"
         class="w-full h-full object-cover"
       />
     </aside>
